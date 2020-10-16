3b729262363566acb7c7edba5884ede5a3a75e52
Karan Gajjar <karangajjar.lp@gmail.com>
2020-10-15 20:51:37 -0400

    first commit

---
+++ app/src/main/java/com/kanulp/calcapp/CalcActivity.kt
@@ -0,0 +1,11 @@
+package com.kanulp.calcapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CalcActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calc)
+    }
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+# Android Calculator App UI 
+
+(https://raw.githubusercontent.com/kanulp/AndroidCalc/master/screenshots/kanulp_android_calc.png
+
+(https://raw.githubusercontent.com/kanulp/AndroidCalc/master/screenshots/kanulp_android_calc2.png
